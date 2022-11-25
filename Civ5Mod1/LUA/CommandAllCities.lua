e8166aef22ad7d19f0e940242d18c69479058be6
LambFerret <dignrjrl12@gmail.com>
2022-11-25 20:03:44 +0900

    init

---
+++ Civ5Mod1/LUA/CommandAllCities.lua
@@ -0,0 +1,48 @@
+-- CommandAllCities
+-- Author: LambFerret
+-- DateCreated: 11/22/2022 3:57:47 PM
+--------------------------------------------------------------
+local DebugEnabled = true;
+local DebugPrint = nil;
+local buildingsInfo = {};
+for row in GameInfo.Buildings() do
+   buildingsInfo[row.ID] = row.Description
+end
+
+if (DebugEnabled) then
+   DebugPrint = function(name, string)
+      print("-------" .. name .. "--------")
+      print(dump(string))
+      print("-------------------")
+   end;
+else
+   DebugPrint = function(name, string) end;
+end
+function dump(o)
+   if type(o) == 'table' then
+      local s = '{ '
+      for k, v in pairs(o) do
+         if type(k) ~= 'number' then k = '"' .. k .. '"' end
+         s = s .. '[' .. k .. '] = ' .. dump(v) .. ',' .. "\n"
+      end
+      return s .. '} '
+   else
+      return tostring(o)
+   end
+end
+
+function gatherListofBuildings(iPlayer)
+   local canConstructMap = {};
+   if (iPlayer ~= 0) then return end
+   for city in Players[iPlayer]:Cities() do
+      for buildingID, v in pairs(buildingsInfo) do
+         if (city:CanConstruct(buildingID)) then
+            canConstructMap[buildingID] = v
+         end
+      end
+   end
+   DebugPrint("cac", canConstructMap)
+   return canConstructMap;
+end
+
+GameEvents.PlayerDoTurn.Add(gatherListofBuildings)
---
+++ Civ5Mod1/SQL/BuildingQueue.sql
@@ -0,0 +1,2 @@
+-- Insert SQL Rules Here 
+update Buildings set happiness=10 where type = 'BUILDING_MONUMENT';
